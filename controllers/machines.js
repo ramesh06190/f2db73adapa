a49d21bcd03e530df72cb13e72ba074ba916b399
ramesh06190 <apvs.ramesh@gmail.com>
2022-11-16 09:06:19 -0600

    Partially updated machine create pug

--- controllers/machines.js
+++ controllers/machines.js
@@ -113,3 +113,16 @@ exports.machines_view_one_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
 };
+// Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.machines_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('machinescreate', { title: 'Machine Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/machines.js
+++ routes/machines.js
@@ -7,4 +7,7 @@ router.get('/', machines_controlers.machines_view_all_Page );
 
 /* GET detail machine page */
 router.get('/detail', machines_controlers.machines_view_one_Page);
+/* GET create machine page */
+router.get('/create', machines_controlers.machines_create_Page);
+
 module.exports = router;
