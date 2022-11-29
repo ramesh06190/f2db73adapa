0dff30b84ed91a871dccdb9c1bcd05c80cba2f5e
ramesh06190 <apvs.ramesh@gmail.com>
2022-11-28 19:56:06 -0600

    Added Update authentication

--- routes/index.js
+++ routes/index.js
@@ -38,10 +38,8 @@ router.get('/login', function(req, res) {
   res.render('login', { title: 'Machines App Login', user : req.user });
 });
 router.post('/login', passport.authenticate('local'), function(req, res) {
-  res.redirect('/');
-});
-router.get('/logout', function(req, res) {
-  req.logout();
+  if(req.session.returnTo){
+    res.redirect(req.session.returnTo);}
   res.redirect('/');
 });
 router.get('/ping', function(req, res){
--- routes/machines.js
+++ routes/machines.js
@@ -10,7 +10,16 @@ router.get('/detail', machines_controlers.machines_view_one_Page);
 /* GET create machine page */
 router.get('/create', machines_controlers.machines_create_Page);
 /* GET create update page */
-router.get('/update', machines_controlers.machines_update_Page);
+// A little function to check if we have an authorized user and continue on or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+}
+router.get('/update', secured, machines_controlers.machines_update_Page);
 /* GET delete machine page */
 router.get('/delete', machines_controlers.machines_delete_Page);
 
